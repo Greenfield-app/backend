1c639cd49b19ece40fe32119c092055c692aadd9
cabbage <cab226023@gmail.com>
2025-02-09 14:27:13 +0900

    add seed data to food table

--- db/seeds/01_food.js
+++ db/seeds/01_food.js
@@ -8,5 +8,17 @@ exports.seed = async function (knex) {
     { foodname: "ramen", description: "Ramen near me" },
     { foodname: "yakiniku", description: "Yakiniku in Sinjuku" },
     { foodname: "italian", description: "Highly recommended by ..." },
+    { foodname: "Casu Marzu", description: "Ramen near me" },
+    { foodname: "Rocky Mountain oysters", description: "Yakiniku in Sinjuku" },
+    {
+      foodname: "Jellied moose nose",
+      description: "Highly recommended by ...",
+    },
+    { foodname: "Shirako:", description: "Ramen near me" },
+    { foodname: "Fruit bat soup", description: "Yakiniku in Sinjuku" },
+    { foodname: "Chicken Shawarma", description: "Highly recommended by ..." },
+    { foodname: "Miso Soup", description: "Ramen near me" },
+    { foodname: "Fried tarantulas", description: "Yakiniku in Sinjuku" },
+    { foodname: "Bushmeat", description: "Highly recommended by ..." },
   ]);
 };
