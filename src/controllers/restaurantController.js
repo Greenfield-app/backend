c7c7c480c490486b04a151d7001da9e5d7a4f467
Karen Saki Miyazawa <ramos.karensaki@gmail.com>
2025-02-12 14:27:47 +0900

    feat: implement getPhotoOfRestaurant to fetch photo of a restaurant by its photo reference

--- src/controllers/restaurantController.js
+++ src/controllers/restaurantController.js
@@ -1,5 +1,6 @@
-const nearbySearch = require('../util/nearbySearch')
-const getGeocodingData = require('../util/geocoding')
+const nearbySearch = require('../util/nearbySearch');
+const getGeocodingData = require('../util/geocoding');
+const getPhoto = require('../util/photo');
 
 // Function to fetch nearby restaurants based on location
 async function getNearbyRestaurants (req, res) {
@@ -24,4 +25,22 @@ async function getNearbyRestaurants (req, res) {
     }
 }
 
-module.exports = { getNearbyRestaurants };
+// Function to get a photo for a restaurant using its photo reference
+async function getPhotoOfRestaurant (req, res) {
+    const { photo_reference } = req.query;
+    
+    // Check if photo reference is provided
+    if (!photo_reference) {
+        return res.status(400).json({ error: 'Photo reference query parameter is required' });
+    }
+
+    try {
+        // Get the photo using the provided reference
+        const photoUrl = await getPhoto(photo_reference);
+        res.send(photoUrl);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}        
+}
+
+module.exports = { getNearbyRestaurants, getPhotoOfRestaurant };
--- src/routes/restaurantRoutes.js
+++ src/routes/restaurantRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const restaurantController = require('../controllers/restaurantController');
 
 router.get('/nearby', restaurantController.getNearbyRestaurants);
+router.get('/photo', restaurantController.getPhotoOfRestaurant);
+
 
 module.exports = router;
