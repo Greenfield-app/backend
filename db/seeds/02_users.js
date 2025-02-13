885b12eaf623dde97edae3a18c0364d1c8a4b085
bnogimauemura <bnogimauemurak@icloud.com>
2025-02-13 10:14:20 +0900

    updated and fixed seed file

--- db/seeds/02_users.js
+++ db/seeds/02_users.js
@@ -2,29 +2,32 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-
-const EXAMPLE_PWD =
-  "$2a$10$b.wYcezF1WcHilMDwilsNeHtIIyBwifxahhP5fnQnyEVPJP9NppGi";
+const { hashPassword } = require('../../src/util/passwordUtils')
 exports.seed = async function (knex) {
   await knex("users").del();
-  await knex("users").insert([
+
+  const users =[
     {
       username: "Anne",
       email: "example1@mail.com",
       creation_date: knex.fn.now(),
-      password_hashed: EXAMPLE_PWD,
+      password: "anne_123",
     },
     {
       username: "Sam",
       email: "example2@mail.com",
       creation_date: knex.fn.now(),
-      password_hashed: EXAMPLE_PWD,
+      password: "sam1234",
     },
     {
       username: "Alex",
       email: "example3@mail.com",
       creation_date: knex.fn.now(),
-      password_hashed: EXAMPLE_PWD,
+      password: "alex23456",
     },
-  ]);
+  ]
+  for(const user of users) {
+    user.password = await hashPassword(user.password)
+  };
+  await knex('users').insert(users);
 };
